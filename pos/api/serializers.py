ed21771973f2f08ee19c9e12f7d010a56d7781bc
MerrrH1 <mervinhowardd38@gmail.com>
2024-11-02 21:57:47 +0700

    Commit Changes

---
+++ pos/api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from pos_app.models import (
+    User, TableResto
+)
+
+class TableRestoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TableResto
+        fields = ('id', 'code', 'name', 'capacity', 'table_status', 'status')
+        # fields = "__all__"
---
+++ pos/api/views.py
@@ -0,0 +1,100 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from pos_app.models import User, TableResto
+from api.serializers import TableRestoSerializer
+
+class TableRestoListApiView(APIView):
+    def get(self, request, *args, **kwargs):
+        table_restos = TableResto.objects.all()
+        serializer = TableRestoSerializer(table_restos, many = True)
+        return Response(serializer.data, status = status.HTTP_200_OK)
+    
+    def post(self, request, *args, **kwargs):
+        data = {
+            'code' : request.data.get('code'),
+            'name' : request.data.get('name'),
+            'capacity' : request.data.get('capacity')
+        }
+        serializer = TableRestoSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            response = {
+                'status' : status.HTTP_201_CREATED,
+                'message' : 'Data created successfully...',
+                'data' : serializer.data
+            }
+            return Response(response, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+class TableRestoDetailApiView(APIView):
+    def get_object(self, id):
+        try:
+            return TableResto.objects.get(id = id)
+        except TableResto.DoesNotExist:
+            return None
+        
+    def get(self, request, id, *args, **kwargs):
+        table_resto_instances = self.get_object(id)
+        if not table_resto_instances:
+            return Response(
+                {
+                    'status' : status.HTTP_404_NOT_FOUND,
+                    'message' : 'Data does not exists...',
+                    'data' : {}
+                },
+                status = status.HTTP_400_BAD_REQUEST
+            )
+        
+        serializer = TableRestoSerializer(table_resto_instances)
+        response = {
+            'status' : status.HTTP_200_OK,
+            'message' : 'Data retrieved successfully...',
+            'data' : serializer.data
+        }
+        return Response(response, status = status.HTTP_200_OK)
+    
+    def put(self, request, id, *args, **kwargs):
+        table_resto_instance = self.get_object(id = id)
+        if not table_resto_instance:
+            return Response(
+                {
+                    'status' : status.HTTP_404_NOT_FOUND,
+                    'message' : 'Data does not exists...',
+                    'data' : {}
+                }, status = status.HTTP_404_NOT_FOUND
+            )
+        data = {
+            'code' : request.data.get('code'),
+            'name' : request.data.get('name'),
+            'capacity' : request.data.get('capacity'),
+            'table_status' : request.data.get('table_status'),
+            'status' : request.data.get('status')
+        }
+        serializer = TableRestoSerializer(instance = table_resto_instance, data = data, partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            response = {
+                'status' : status.HTTP_200_OK,
+                'message' : 'Data updated successfully',
+                'data' : serializer.data
+            }
+            return Response(response, status = status.HTTP_200_OK)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, id, *args, **kwargs):
+        table_resto_instance = self.get_object(id = id)
+        if not table_resto_instance:
+            return Response(
+                {
+                    'status' : status.HTTP_404_NOT_FOUND,
+                    'message' : 'Data does not exists...',
+                    'data' : {}
+                }, status = status.HTTP_400_BAD_REQUEST
+            )
+        table_resto_instance.delete()
+        response = {
+            'status' : status.HTTP_200_OK,
+            'message' : 'Data deleted successfully...',
+        }
+        return Response(response, status = status.HTTP_200_OK)
